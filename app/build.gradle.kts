f303f5b5845d549867381b385b387bfed92677c1
Jamie Nicol <jamiern@fastmail.com>
2026-03-20 14:35:48 +0000

    remove as many deps as possible from base

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,11 +34,5 @@ android {
 
 dependencies {
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.collection)
 }
