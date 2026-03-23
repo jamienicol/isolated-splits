06fdbad32cfff74ca25c1d1e4a80996738eda330
Jamie Nicol <jamiern@fastmail.com>
2026-03-23 10:57:18 +0000

    disable ABI splits in bundles
    This means that even with bundle installs libjnidispatch ends up in
    the base apk rather than an ABI split one, so even with isolatedSplits
    = true the library can indeed be found

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    bundle {
+        abi {
+            enableSplit = false
+        }
+    }
     dynamicFeatures += ":feature_browser"
 }
 
